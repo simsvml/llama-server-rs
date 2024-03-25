35716a8d272d97a545f76745af55a6fa376ca9a9
simsvml <simsvml@tutanota.com>
2024-03-25 15:59:09 +0000

    add safe wrappers for llama.cpp operations

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,369 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
+use std::ffi::CString;
+use std::marker::PhantomData;
+use std::ptr::{self, NonNull};
+use std::slice;
+use std::str;
+
 
 pub mod ffi {
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
+
+
+pub type LlamaToken = ffi::llama_token;
+pub type LlamaTokenData = ffi::llama_token_data;
+pub type LlamaModelParams = ffi::llama_model_params;
+pub type LlamaContextParams = ffi::llama_context_params;
+
+
+#[derive(Debug)]
+pub struct LlamaModel(NonNull<ffi::llama_model>);
+
+impl LlamaModel {
+    pub fn load_from_file(
+        path: impl Into<Vec<u8>>,
+        model_params: ffi::llama_model_params,
+    ) -> Option<LlamaModel> {
+        unsafe {
+            let name = CString::new(path).unwrap();
+            let ptr = ffi::llama_load_model_from_file(name.as_ptr(), model_params);
+            let ptr = NonNull::new(ptr)?;
+            Some(LlamaModel(ptr))
+        }
+    }
+
+    pub fn as_ptr(&self) -> *mut ffi::llama_model {
+        self.0.as_ptr()
+    }
+
+    /// Tokenize `text`, appending the tokens to `tokens`.  This never grows `tokens`; the caller
+    /// should ensure it has enough free capacity before the call.  On success, returns `Ok` with
+    /// the number of tokens that were added to `tokens`.  On failure (not enough space in
+    /// `tokens`), returns `Err(n)` where `n` is the number of tokens found in `text`.  (In the
+    /// `Err(n)` case, the caller might want to call `tokens.reserve(n)` and try again.)
+    pub fn try_tokenize(
+        &self,
+        text: &str,
+        tokens: &mut Vec<ffi::llama_token>,
+        add_bos: bool,
+        special: bool,
+    ) -> Result<usize, usize> {
+        unsafe {
+            let space = tokens.spare_capacity_mut();
+            let r = ffi::llama_tokenize(
+                self.as_ptr(),
+                text.as_ptr().cast::<i8>(),
+                text.len().try_into().unwrap(),
+                space.as_mut_ptr().cast::<ffi::llama_token>(),
+                space.len().try_into().unwrap(),
+                add_bos,
+                special,
+            );
+            if r < 0 {
+                // Not enough space in `tokens`.
+                return Err((-r).try_into().unwrap());
+            }
+
+            let added = usize::try_from(r).unwrap();
+            // Otherwise, `r` tokens were appended to `tokens`.
+            debug_assert!(added <= space.len());
+            debug_assert_eq!(space.len(), tokens.capacity() - tokens.len());
+            // This addition can't overflow because we know the sum doesn't exceed
+            // `tokens.capacity()`.
+            tokens.set_len(tokens.len() + added);
+            Ok(added)
+        }
+    }
+
+    pub fn try_token_to_piece(
+        &self,
+        token: ffi::llama_token,
+        buf: &mut Vec<u8>,
+    ) -> Result<usize, usize> {
+        unsafe {
+            let space = buf.spare_capacity_mut();
+            let r = ffi::llama_token_to_piece(
+                self.as_ptr(),
+                token,
+                space.as_mut_ptr().cast::<i8>(),
+                space.len().try_into().unwrap(),
+            );
+            if r < 0 {
+                // Not enough space in `buf`.
+                return Err((-r).try_into().unwrap());
+            }
+
+            let added = usize::try_from(r).unwrap();
+            // Otherwise, `r` bytes were appended to `buf`.
+            debug_assert!(added <= space.len());
+            debug_assert_eq!(space.len(), buf.capacity() - buf.len());
+            // This addition can't overflow because we know the sum doesn't exceed
+            // `buf.capacity()`.
+            buf.set_len(buf.len() + added);
+            Ok(added)
+        }
+    }
+
+    pub fn try_token_to_piece_str<'a>(
+        &self,
+        token: ffi::llama_token,
+        buf: &'a mut Vec<u8>,
+    ) -> Result<&'a str, usize> {
+        let old_len = buf.len();
+        self.try_token_to_piece(token, buf)?;
+        let new_len = buf.len();
+        let s = str::from_utf8(&buf[old_len..new_len]).unwrap();
+        Ok(s)
+    }
+}
+
+impl Drop for LlamaModel {
+    fn drop(&mut self) {
+        unsafe {
+            ffi::llama_free_model(self.as_ptr());
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct LlamaContext<'a> {
+    ptr: NonNull<ffi::llama_context>,
+    /// Indicates whether logits are available for a given token index (as used with
+    /// `llama_get_logits_ith`).  This is initialized from `batch.logits()` when calling
+    /// `self.decode(&batch)`.
+    logits_available: Vec<bool>,
+    _marker: PhantomData<&'a LlamaModel>,
+}
+
+impl<'a> LlamaContext<'a> {
+    pub fn with_model(
+        model: &'a LlamaModel,
+        context_params: ffi::llama_context_params,
+    ) -> Option<LlamaContext<'a>> {
+        unsafe {
+            let ptr = ffi::llama_new_context_with_model(model.as_ptr(), context_params);
+            let ptr = NonNull::new(ptr)?;
+            Some(LlamaContext {
+                ptr,
+                logits_available: Vec::new(),
+                _marker: PhantomData,
+            })
+        }
+    }
+
+    pub fn as_ptr(&self) -> *mut ffi::llama_context {
+        self.ptr.as_ptr()
+    }
+
+    pub fn n_ctx(&self) -> usize {
+        unsafe {
+            ffi::llama_n_ctx(self.as_ptr()).try_into().unwrap()
+        }
+    }
+
+    /// Process a batch of tokens.
+    ///
+    /// This takes `&mut`, so it can't be called while a borrow from `logits_ith` is outstanding.
+    pub fn decode(&mut self, batch: &LlamaBatch) -> Result<(), String> {
+        unsafe {
+            self.logits_available = Vec::with_capacity(batch.len());
+            let r = ffi::llama_decode(self.as_ptr(), batch.as_raw());
+            match r {
+                0 => {},
+                1 => return Err(format!(
+                        "couldn't find kv slot for batch of size {}", batch.len())),
+                _ => return Err(format!("llama_decode failed: {}", r)),
+            }
+            self.logits_available.extend(batch.logits().iter().map(|&x| x != 0));
+            Ok(())
+        }
+    }
+
+    /// Obtain the logits for token `i` from the last batch.
+    ///
+    /// This borrows from `self`, which prevents `decode()` (which might overwrite the logits
+    /// buffer) from being called while the returned slice is still in use.
+    pub fn logits_ith<'b>(&'b self, i: usize) -> &'b [f32] {
+        unsafe {
+            assert!(self.logits_available[i]);
+
+            let model = ffi::llama_get_model(self.as_ptr());
+            let n_vocab = ffi::llama_n_vocab(model);
+            let ptr = ffi::llama_get_logits_ith(self.as_ptr(), i.try_into().unwrap());
+            slice::from_raw_parts(
+                ptr,
+                n_vocab.try_into().unwrap(),
+            )
+        }
+    }
+
+    pub fn sample_softmax(&self, candidates: &mut LlamaTokenDataArray) {
+        unsafe {
+            ffi::llama_sample_softmax(self.as_ptr(), candidates.as_mut_ptr())
+        }
+    }
+
+    pub fn sample_token(&self, candidates: &mut LlamaTokenDataArray) -> ffi::llama_token {
+        unsafe {
+            ffi::llama_sample_token(self.as_ptr(), candidates.as_mut_ptr())
+        }
+    }
+}
+
+impl<'a> Drop for LlamaContext<'a> {
+    fn drop(&mut self) {
+        unsafe {
+            ffi::llama_free(self.as_ptr());
+        }
+    }
+}
+
+
+pub struct LlamaBatch {
+    batch: ffi::llama_batch,
+    capacity: usize,
+}
+
+impl LlamaBatch {
+    pub fn new(n_tokens: usize) -> LlamaBatch {
+        unsafe {
+            let batch = ffi::llama_batch_init(
+                n_tokens.try_into().unwrap(),
+                0,  // embd
+                1,  // n_seq_max
+            );
+            LlamaBatch {
+                batch,
+                capacity: n_tokens,
+            }
+        }
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.capacity
+    }
+
+    pub fn len(&self) -> usize {
+        // This cast is okay because 0 <= n_tokens <= capacity
+        self.batch.n_tokens as usize
+    }
+
+    pub fn clear(&mut self) {
+        self.batch.n_tokens = 0;
+    }
+
+    pub fn push(
+        &mut self,
+        token: ffi::llama_token,
+        pos: usize,
+        seq_id: ffi::llama_seq_id,
+        logits: bool,
+    ) {
+        unsafe {
+            assert!(self.len() < self.capacity());
+            let i = self.len();
+            *self.batch.token.add(i) = token;
+            *self.batch.pos.add(i) = pos.try_into().unwrap();
+            *self.batch.n_seq_id.add(i) = 1;
+            *(*self.batch.seq_id.add(i)).add(0) = seq_id;
+            *self.batch.logits.add(i) = logits as i8;
+
+            self.batch.n_tokens += 1;
+        }
+    }
+
+    pub fn logits<'a>(&'a self) -> &'a [i8] {
+        unsafe {
+            slice::from_raw_parts(
+                self.batch.logits,
+                self.len(),
+            )
+        }
+    }
+
+    pub fn as_raw(&self) -> ffi::llama_batch {
+        self.batch
+    }
+
+    pub fn into_inner(self) -> ffi::llama_batch {
+        self.batch
+    }
+}
+
+impl Drop for LlamaBatch {
+    fn drop(&mut self) {
+        unsafe {
+            ffi::llama_batch_free(self.batch);
+        }
+    }
+}
+
+
+pub struct LlamaTokenDataArray<'a> {
+    raw: ffi::llama_token_data_array,
+    _marker: PhantomData<&'a mut [ffi::llama_token_data]>,
+}
+
+impl<'a> LlamaTokenDataArray<'a> {
+    pub fn new(buf: &'a mut [ffi::llama_token_data]) -> LlamaTokenDataArray<'a> {
+        LlamaTokenDataArray {
+            raw: ffi::llama_token_data_array {
+                data: buf.as_mut_ptr(),
+                size: buf.len().try_into().unwrap(),
+                sorted: false,
+            },
+            _marker: PhantomData,
+        }
+    }
+
+    pub fn as_mut_ptr(&mut self) -> *mut ffi::llama_token_data_array {
+        ptr::addr_of_mut!(self.raw)
+    }
+}
+
+
+pub fn default_model_params() -> ffi::llama_model_params {
+    unsafe { ffi::llama_model_default_params() }
+}
+
+pub fn default_context_params() -> ffi::llama_context_params {
+    unsafe { ffi::llama_context_default_params() }
+}
+
+
+pub fn dump_batch(batch: ffi::llama_batch) {
+    unsafe {
+        unsafe fn mk_slice<'a, T>(ptr: *const T, len: usize) -> &'a [T] {
+            if ptr.is_null() {
+                &[]
+            } else {
+                std::slice::from_raw_parts(ptr, len)
+            }
+        }
+
+        eprintln!("batch: {} tokens", batch.n_tokens);
+        let n = usize::try_from(batch.n_tokens).unwrap();
+
+        let token = mk_slice(batch.token, n);
+        let pos = mk_slice(batch.pos, n);
+        let n_seq_id = mk_slice(batch.n_seq_id, n);
+        let seq_id = mk_slice(batch.seq_id, n);
+        let logits = mk_slice(batch.logits, n);
+
+        let iter = token.iter()
+            .zip(pos.iter())
+            .zip(n_seq_id.iter())
+            .zip(seq_id.iter())
+            .zip(logits.iter());
+        for ((((&token, &pos), &n_seq_id), &seq_id), &logits) in iter {
+            let seq_id = mk_slice(seq_id, n_seq_id.try_into().unwrap());
+            eprintln!("  token {}, pos {}, n_seq_id {}, seq_id {:?}, logits {}",
+                token, pos, n_seq_id, seq_id, logits);
+        }
+    }
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::ffi::{CStr, CString};
-use std::mem;
 use clap::{Command, Arg, ArgMatches, value_parser};
-use libc;
-use llama_server_rs::ffi;
+use llama_server_rs::{
+    LlamaModel, default_model_params, LlamaContext, default_context_params, LlamaBatch,
+    LlamaTokenData, LlamaTokenDataArray,
+};
+    
 
 fn parse_args() -> ArgMatches {
     Command::new("llama-server-rs")
@@ -17,115 +18,59 @@ fn main() {
     let args = parse_args();
 
     // Load model
-    let mut model_params = unsafe { ffi::llama_model_default_params() };
+    let mut model_params = default_model_params();
     model_params.n_gpu_layers = 999;
 
     let model_path: String = args.get_one::<String>("model").unwrap().clone();
-    let model = unsafe {
-        let name = CString::new(model_path).unwrap();
-        ffi::llama_load_model_from_file(name.as_ptr(), model_params)
-    };
+    let model = LlamaModel::load_from_file(model_path, model_params).unwrap();
     eprintln!("model = {:?}", model);
 
     // Create context
-    let mut context_params = unsafe { ffi::llama_context_default_params() };
+    let mut context_params = default_context_params();
     context_params.n_ctx = args.get_one::<usize>("ctx_size").unwrap().clone().try_into().unwrap();
-    let ctx = unsafe { ffi::llama_new_context_with_model(model, context_params) };
+    let mut ctx = LlamaContext::with_model(&model, context_params).unwrap();
     eprintln!("ctx = {:?}", ctx);
 
     // Tokenize input
     let input = "[INST] Is Rust a compiled language? [/INST]";
-    let n_ctx = usize::try_from(unsafe { ffi::llama_n_ctx(ctx) }).unwrap();
-    let mut tokens = vec![0; n_ctx];
-
-    unsafe {
-        let r = ffi::llama_tokenize(
-            model,
-            input.as_ptr().cast::<i8>(),
-            input.len().try_into().unwrap(),
-            tokens.as_mut_ptr(),
-            tokens.capacity().try_into().unwrap(),
-            true,   // add_bos
-            true,   // special
-        );
-        assert!(r >= 0, "input has too many tokens: {}", r);
-        tokens.set_len(r.try_into().unwrap());
-    }
+    let mut tokens = Vec::with_capacity(ctx.n_ctx());
+    model.try_tokenize(input, &mut tokens, /* add_bos: */ true, /* special: */ true)
+        .unwrap_or_else(|n| panic!("input has too many tokens: {}", n));
     eprintln!("tokens = {:?}", tokens);
 
     // Prompt processing
-    let mut batch = unsafe {
-        ffi::llama_batch_init(
-            tokens.len().try_into().unwrap(),
-            0,  // embd
-            1,  // n_seq_max
-        )
-    };
-
+    let mut batch = LlamaBatch::new(tokens.len());
     for (i, &tok) in tokens.iter().enumerate() {
-        unsafe {
-            *batch.token.add(i) = tok;
-            *batch.pos.add(i) = i.try_into().unwrap();
-            *batch.n_seq_id.add(i) = 1;
-            *batch.seq_id.add(i) = libc::malloc(mem::size_of::<ffi::llama_seq_id>()
-                .try_into().unwrap()).cast::<i32>();
-            *(*batch.seq_id.add(i)) = 0;
-            *batch.logits.add(i) = (i == tokens.len() - 1) as _;
-        }
+        let last = i == tokens.len() - 1;
+        batch.push(tok, i, /* seq_id: */ 0, last);
     }
-    batch.n_tokens = tokens.len().try_into().unwrap();
 
-    let r = unsafe { ffi::llama_decode(ctx, batch) };
-    match r {
-        0 => {},
-        1 => panic!("couldn't find kv slot for batch of size {}", batch.n_tokens),
-        _ => panic!("llama_decode failed"),
-    }
+    ctx.decode(&batch).unwrap();
     eprintln!("llama_decode ok");
 
 
     // Sampling
-    let n_vocab = usize::try_from(unsafe { ffi::llama_n_vocab(model) }).unwrap();
     let mut pos = tokens.len();
     let mut logits_index = pos - 1;
+    let mut token_buf = Vec::with_capacity(64);
     loop {
         // Sample token
-        let logits = unsafe {
-            ffi::llama_get_logits_ith(
-                ctx,
-                logits_index.try_into().unwrap(),
-            )
-        };
-        let mut token_data = Vec::with_capacity(n_vocab);
-        for i in 0 .. n_vocab {
-            let logit = unsafe { *logits.add(i) };
-            token_data.push(ffi::llama_token_data {
+        let logits = ctx.logits_ith(logits_index);
+        let mut token_data = logits.iter().enumerate().map(|(i, &logit)| {
+            LlamaTokenData {
                 id: i.try_into().unwrap(),
                 logit,
                 p: 0.,
-            });
-        }
-        let mut candidates = ffi::llama_token_data_array {
-            data: token_data.as_mut_ptr(),
-            size: token_data.len().try_into().unwrap(),
-            sorted: false,
-        };
+            }
+        }).collect::<Vec<_>>();
 
-        unsafe { ffi::llama_sample_softmax(ctx, &mut candidates) };
-        let token = unsafe { ffi::llama_sample_token(ctx, &mut candidates) };
+        let mut candidates = LlamaTokenDataArray::new(&mut token_data);
 
-        let token_str = unsafe {
-            let mut buf = Vec::<u8>::with_capacity(64);
-            let r = ffi::llama_token_to_piece(
-                model,
-                token,
-                buf.as_mut_ptr().cast::<i8>(),
-                buf.capacity().try_into().unwrap(),
-            );
-            assert!(r >= 0, "sampled token is too long for buffer: {}", r);
-            buf.set_len(r.try_into().unwrap());
-            String::from_utf8(buf).unwrap()
-        };
+        ctx.sample_softmax(&mut candidates);
+        let token = ctx.sample_token(&mut candidates);
+
+        token_buf.clear();
+        let token_str = model.try_token_to_piece_str(token, &mut token_buf).unwrap();
 
         eprint!("{}", token_str);
         //eprintln!("sampled token {} = {:?}", token, token_str);
@@ -135,29 +80,12 @@ fn main() {
         }
 
         // Prepare next batch
-        unsafe {
-            *batch.logits.add(logits_index) = 0;
-
-            batch.n_tokens = 1;
-            *batch.token = token;
-            *batch.pos = pos.try_into().unwrap();
-            if *batch.n_seq_id == 0 {
-                *batch.n_seq_id = 1;
-                *batch.seq_id = libc::malloc(mem::size_of::<ffi::llama_seq_id>()
-                    .try_into().unwrap()).cast::<i32>();
-            }
-            *(*batch.seq_id) = 0;
-            *batch.logits = true as _;
-        }
+        batch.clear();
+        batch.push(token, /* pos: */ pos, /* seq_id: */ 0, true);
         pos += 1;
         logits_index = 0;
 
-        let r = unsafe { ffi::llama_decode(ctx, batch) };
-        match r {
-            0 => {},
-            1 => panic!("couldn't find kv slot for batch of size {}", batch.n_tokens),
-            _ => panic!("llama_decode failed"),
-        }
+        ctx.decode(&batch).unwrap();
         //eprintln!("llama_decode ok");
     }
     eprintln!();
